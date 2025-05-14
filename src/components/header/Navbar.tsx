e740d3dbae57be6f957758d72ced0ff445c72cdf
percxno <161191884+percxno@users.noreply.github.com>
2025-05-14 20:39:37 +0530

    feat: wrap logo image in Link component for improved navigation

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Navbar = () => {
   return (
     <section className="h-20 flex items-center border-l border-r w-full font-light">
       <div className="flex items-center px-5 border-r h-full cursor-pointer">
-        <Image
-          src="assets/logos/full-logo.svg"
-          alt="logo"
-          width={150}
-          height={32}
-        />
+        <Link href="/">
+          <Image
+            src="assets/logos/full-logo.svg"
+            alt="logo"
+            width={150}
+            height={32}
+          />
+        </Link>
       </div>
     </section>
   );
