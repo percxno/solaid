fd5a642ed5a5a7c8734897c9c351ded233cb51a6
percxno <161191884+percxno@users.noreply.github.com>
2025-05-14 21:20:15 +0530

    style: format button components for improved readability

--- src/components/header/NavbarCta.tsx
+++ src/components/header/NavbarCta.tsx
@@ -4,8 +4,18 @@ import { MoveUpRightIcon } from 'lucide-react';
 export function NavbarCta() {
   return (
     <section className="w-fit flex items-center px-5 gap-5">
-      <button className="border-r h-full pr-5 cursor-pointer">Connect</button>
-      <button className="h-full cursor-pointer flex items-center">
+      <button
+        className="border-r h-full pr-5 cursor-pointer flex items-center
+        text-white/50 hover:text-primary transition-colors duration-200
+      "
+      >
+        Connect
+      </button>
+      <button
+        className="h-full cursor-pointer flex items-center
+      text-white/50 hover:text-primary transition-colors duration-200
+      "
+      >
         Start a Fundraiser
         <MoveUpRightIcon className="ml-2 font-extralight" size={16} />
       </button>
