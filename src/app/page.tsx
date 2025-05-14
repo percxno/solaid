f4867f75c67b1bd1ad3b7e0a1e89cf40f772ad22
percxno <161191884+percxno@users.noreply.github.com>
2025-05-15 00:42:55 +0530

    feat: add Ripple component for enhanced visual effects and integrate it into the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image';
+import { MoveUpRightIcon } from 'lucide-react';
+
+import { FlickeringGrid } from '@/components/magicui/flickering-grid';
+import { Ripple } from '@/components/magicui/ripple';
 
 export default function Home() {
   return (
-    <main className="main-container container">
-      <section className="flex mt-32 gap-16 justify-between">
+    <main className="main-container container items-center flex-col">
+      <section className="flex mt-32 gap-16 justify-between bg-transparent">
         <h1 className="text-7xl w-1/2 whitespace-nowrap">
           Emergency funds.
           <br />
@@ -19,6 +22,16 @@ export default function Home() {
           medical bills, community needs, and more.
         </p>
       </section>
+      <div className="relative h-[500px] w-[700px] flex justify-center">
+        <Ripple className="absolute -top-[250px]" />
+        <button
+          className="cursor-pointer flex items-center px-10 uppercase h-[32px] mt-[110px] font-light
+      text-white/80 hover:text-primary hover:underline transition-colors duration-200
+      "
+        >
+          START FUNDRAISING
+        </button>
+      </div>
     </main>
   );
 }
