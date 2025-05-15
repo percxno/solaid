7ece36f04b1788c7078860ccf8776bc48bae1714
percxno <161191884+percxno@users.noreply.github.com>
2025-05-15 13:51:21 +0530

    feat(NavbarCta): implement wallet connection and display truncated public key

--- src/components/header/NavbarCta.tsx
+++ src/components/header/NavbarCta.tsx
@@ -1,17 +1,37 @@
-import React from 'react';
+'use client';
+import React, { useMemo } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
 import { MoveUpRightIcon } from 'lucide-react';
 
 import { FlickeringGrid } from '@/components/magicui/flickering-grid';
+import { truncate } from '@/lib/utils';
+
+import { TextScramble } from '../../../components/motion-primitives/text-scramble';
 
 export function NavbarCta() {
+  const { publicKey, select, disconnect, wallets, connected } = useWallet();
+
+  const rawKey = useMemo(() => publicKey?.toBase58() ?? '', [publicKey]);
+
+  const displayKey = useMemo(() => (rawKey ? truncate(rawKey) : ''), [rawKey]);
+
+  const handleConnect = async () => {
+    select(wallets[0].adapter.name);
+  };
+
+  const handleDisconnect = async () => {
+    await disconnect();
+  };
+
   return (
     <section className="w-fit flex items-center">
       <button
-        className="relative border-r h-full px-10 cursor-pointer flex items-center
+        className="relative border-r h-full px-10 cursor-pointer flex items-center w-[144px]
         text-white/80 hover:text-primary transition-colors duration-200
       "
+        onClick={!connected ? handleConnect : handleDisconnect}
       >
-        Connect
+        {!connected ? 'Connect' : <TextScramble>{displayKey}</TextScramble>}
         <FlickeringGrid
           className="absolute top-0 left-0 w-full h-full opacity-0 hover:opacity-75 transition-opacity duration-200"
           squareSize={2}
