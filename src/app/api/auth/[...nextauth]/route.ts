539d8013d046cc7f0eb552b40a8ea6d480ced2bf
percxno <161191884+percxno@users.noreply.github.com>
2025-05-17 15:31:12 +0530

    fix: add auth secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,32 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google";
-import TwitterProvider from "next-auth/providers/twitter";
-
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import TwitterProvider from 'next-auth/providers/twitter';
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-          profile(profile) {
-            return {
-              id: profile.sub,
-              name: profile.name,
-              email: profile.email,
-              image: profile.picture,
-              firstName: profile.given_name,
-              lastName: profile.family_name,
-              locale: profile.locale,
-              emailVerified: profile.email_verified,
-            };
-          },
-        }),
-        TwitterProvider({
-          clientId: process.env.TWITTER_CLIENT_ID!,
-          clientSecret: process.env.TWITTER_CLIENT_SECRET!,
-          version: "2.0",
-        }),
-      ],
-    
-})
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          firstName: profile.given_name,
+          lastName: profile.family_name,
+          locale: profile.locale,
+          emailVerified: profile.email_verified,
+        };
+      },
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+      version: '2.0',
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
