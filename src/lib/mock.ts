818fde7aea9daf13fe01543346a35ed7bdc5b7d2
Apra487 <apra487@gmail.com>
2025-05-17 12:06:06 +0530

    add mock data for animal and category

--- src/lib/mock.ts
+++ src/lib/mock.ts
@@ -12,84 +12,84 @@ export const mockCampaigns: Campaign[] = [
   {
     id: '1',
     category: 'animal',
-    beneficiary: 'Safari Animal Rescue',
-    amount: 5000,
+    beneficiary: 'Karen Hart',
+    amount: 10000,
     title:
-      'Save the Lost Cub blah balh blah blah adjfkja fh jkhasd fj hfjahsd jadsfkjha ajksdf sg',
+      'Support Gunnar’s Parathyroid Tumor Surgery',
     story:
-      'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
-    imageUrl: '/images/lion-cub.png',
+      'Gunnar had dental surgery recently and it was discovered that he had high calcium levels. Upon further testing and a malignancy panel, it was discovered he’d need surgery to remove a parathyroid tumor. He had surgery here in Little Rock on Monday, May 12, 2025, to remove his parathyroid tumor. Unfortunately, the tumor was large and vascular, so the surgeon is concerned that it could be malignant. We should get his biopsy results back next Monday. Due to the size of the parathyroid tumor, the surgeon was not able to perform surgery on Monday to also remove Gunnar’s bladder stones. He has a large amount of bladder stones that will be removed in an upcoming surgery in the near future. These bladder stones are the result of his parathyroid tumor producing too much calcium for his body. After having his parathyroid gland removed, Gunnar will go in for frequent monitoring to see what his calcium levels are at. We can’t let his calcium get too low or he will need an IV of calcium. He went in yesterday to have two calcium checks, and he will go back on Friday to have his levels checked twice again.  ',
+    imageUrl: '/images/dog.webp',
   },
   {
     id: '2',
-    category: 'animal',
-    beneficiary: 'Wildlife Protection Fund',
+    category: 'community',
+    beneficiary: 'Michelle Vanallsburg',
     amount: 7500,
-    title: 'Protect Endangered Elephants',
+    title: 'Legal Fees for Librarian',
     story:
-      'Elephant populations are dwindling... Support our anti-poaching patrols.',
-    imageUrl: '/images/elephants.png',
+      'Ms. Beachler has been a dedicated and loyal school staff member for 37 years, devoted to providing all students with outstanding educational opportunities. She has dedicated her career as a librarian to providing high quality literature for every single student. Although she is highly respected by students, colleagues, and the community, she has been the target of harmful and false allegations. A local individual associated with Moms for Liberty has publicly called her derogatory names, including “smut peddler” and “groomer,” in an apparent attempt to tarnish her reputation and undermine her career. Additionally, the individual has spread disinformation repeatedly telling the community that Ms. Beachler gives pornography to young children. All of these false accusations have been devastating to Ms. Beachler and her family.',
+    imageUrl: '/images/library.webp',
   },
-  {
-    id: '3',
-    category: 'animal',
-    beneficiary: 'Safari Animal Rescue',
-    amount: 5000,
-    title: 'Save the Lost Cub',
-    story:
-      'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
-    imageUrl: '/images/lion-cub.png',
-  },
-  {
-    id: '4',
-    category: 'animal',
-    beneficiary: 'Wildlife Protection Fund',
-    amount: 7500,
-    title: 'Protect Endangered Elephants',
-    story:
-      'Elephant populations are dwindling... Support our anti-poaching patrols.',
-    imageUrl: '/images/elephants.png',
-  },
-  {
-    id: '5',
-    category: 'animal',
-    beneficiary: 'Safari Animal Rescue',
-    amount: 5000,
-    title: 'Save the Lost Cub',
-    story:
-      'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
-    imageUrl: '/images/lion-cub.png',
-  },
-  {
-    id: '6',
-    category: 'animal',
-    beneficiary: 'Wildlife Protection Fund',
-    amount: 7500,
-    title: 'Protect Endangered Elephants',
-    story:
-      'Elephant populations are dwindling... Support our anti-poaching patrols.',
-    imageUrl: '/images/elephants.png',
-  },
-  {
-    id: '7',
-    category: 'animal',
-    beneficiary: 'Safari Animal Rescue',
-    amount: 5000,
-    title: 'Save the Lost Cub',
-    story:
-      'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
-    imageUrl: '/images/lion-cub.png',
-  },
-  {
-    id: '8',
-    category: 'animal',
-    beneficiary: 'Wildlife Protection Fund',
-    amount: 7500,
-    title: 'Protect Endangered Elephants',
-    story:
-      'Elephant populations are dwindling... Support our anti-poaching patrols.',
-    imageUrl: '/images/elephants.png',
-  },
+  // {
+  //   id: '3',
+  //   category: 'animal',
+  //   beneficiary: 'Safari Animal Rescue',
+  //   amount: 5000,
+  //   title: 'Save the Lost Cub',
+  //   story:
+  //     'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
+  //   imageUrl: '/images/lion-cub.png',
+  // },
+  // {
+  //   id: '4',
+  //   category: 'animal',
+  //   beneficiary: 'Wildlife Protection Fund',
+  //   amount: 7500,
+  //   title: 'Protect Endangered Elephants',
+  //   story:
+  //     'Elephant populations are dwindling... Support our anti-poaching patrols.',
+  //   imageUrl: '/images/elephants.png',
+  // },
+  // {
+  //   id: '5',
+  //   category: 'animal',
+  //   beneficiary: 'Safari Animal Rescue',
+  //   amount: 5000,
+  //   title: 'Save the Lost Cub',
+  //   story:
+  //     'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
+  //   imageUrl: '/images/lion-cub.png',
+  // },
+  // {
+  //   id: '6',
+  //   category: 'animal',
+  //   beneficiary: 'Wildlife Protection Fund',
+  //   amount: 7500,
+  //   title: 'Protect Endangered Elephants',
+  //   story:
+  //     'Elephant populations are dwindling... Support our anti-poaching patrols.',
+  //   imageUrl: '/images/elephants.png',
+  // },
+  // {
+  //   id: '7',
+  //   category: 'animal',
+  //   beneficiary: 'Safari Animal Rescue',
+  //   amount: 5000,
+  //   title: 'Save the Lost Cub',
+  //   story:
+  //     'A young lion cub was found orphaned... Help us provide medical care and rehabilitation. dsafhjka dhsf dshfjkhsda kjfkjsd fgdsakgf jdhsagf ajgsd ',
+  //   imageUrl: '/images/lion-cub.png',
+  // },
+  // {
+  //   id: '8',
+  //   category: 'animal',
+  //   beneficiary: 'Wildlife Protection Fund',
+  //   amount: 7500,
+  //   title: 'Protect Endangered Elephants',
+  //   story:
+  //     'Elephant populations are dwindling... Support our anti-poaching patrols.',
+  //   imageUrl: '/images/elephants.png',
+  // },
   {
     id: '9',
     category: 'education',
