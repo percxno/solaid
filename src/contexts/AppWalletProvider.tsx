a56b533788159ba5a485d07916e60817a70af10d
percxno <161191884+percxno@users.noreply.github.com>
2025-05-18 17:11:45 +0530

    refactor: switch network from Devnet to Mainnet in AppWalletProvider

--- src/contexts/AppWalletProvider.tsx
+++ src/contexts/AppWalletProvider.tsx
@@ -14,7 +14,7 @@ export default function AppWalletProvider({
 }: {
   children: React.ReactNode;
 }) {
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
   const wallets = useMemo(() => [new PhantomWalletAdapter()], [network]);
 
